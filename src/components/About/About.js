8c49388e59417cc4d25103b265f77aa09c9dce84
itsaalexk <alexkononenko6@protonmail.com>
2023-02-18 10:58:52 +0100

    fixed a typo in About Me component

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,25 +1,22 @@
-
 import GitHubIcon from '@material-ui/icons/GitHub'
 import LinkedInIcon from '@material-ui/icons/LinkedIn'
 import { about } from '../../portfolio'
 import './About.css'
-import 'animate.css';
+import 'animate.css'
 
 const About = () => {
-  const {photo,name, role, description, resume, social } = about
+  const { photo, name, role, description, resume, social } = about
 
   return (
     <div className='about center animate__animated animate__backInRight'>
-      {photo && (
-       <img height="200px" alt="Avatar placeholder" src={photo} />
-      )}
+      {photo && <img height='200px' alt='Avatar placeholder' src={photo} />}
       {name && (
         <h1>
           Hi, I am <span className='about__name'>{name}.</span>
         </h1>
       )}
 
-      {role && <h2 className='about__role'>A {role}.</h2>}
+      {role && <h2 className='about__role'>{role}.</h2>}
       <p className='about__desc'>{description && description}</p>
 
       <div className='about__contact center'>
