ef6da00a5f3d79db464c0798920ba26310490441
itsaalexk <alexkononenko6@protonmail.com>
2023-03-06 16:10:30 +0100

    removed Footer name

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,20 +1,14 @@
-
 import './Footer.css'
-// Hacer un fetch a GITHUB Api para ver contribuiciones 
+// Hacer un fetch a GITHUB Api para ver contribuiciones
 const Footer = () => (
-  
-
-
   <footer className='footer'>
     <a
       href='https://github.com/itsaalexk/main-site'
       className='link footer__link'
     >
-      Created By Alex Kononenko
+      Created By
     </a>
-    
   </footer>
 )
-
 
 export default Footer
